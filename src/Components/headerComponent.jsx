733a5d08a1426a3abf4fbff0821e19208dec6b00
Prince <princegujjar1.pp@gmail.com>
2022-07-19 17:50:47 +0530

    addedlogin modal & form validation in contact us

--- src/Components/headerComponent.jsx
+++ src/Components/headerComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,10 +6,18 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import {NavLink} from "react-router-dom";
 import './header.css';
+import {Modal,ModalHeader,ModalBody} from 'reactstrap'
 
- class Header extends Component{
-    render(){
+ const Header=()=> {
+  
+      const[isModalOpen,setIsModalOpen]=useState(false);
+
+      const toggleModal=()=>{
+        return( setIsModalOpen({isModalOpen:!isModalOpen}));
+       
+      }
         return(
+          <>
          
             <Navbar bg="transparent" expand="sm">
       <Container fluid>
@@ -25,7 +33,8 @@ import './header.css';
             < NavLink className="b"  to='/menu'>Menu</NavLink>
             < NavLink className="b"  to='/About'>About Us</NavLink>
             < NavLink className="b"  to='/contact'>Contact Us</NavLink>
-           
+
+      
             
           </Nav>
           <Form className="d-flex" >
@@ -35,12 +44,40 @@ import './header.css';
             
               aria-label="Search"
             />
-            <Button className="sButton" variant="outline-success">Search</Button>
+            <Button className="sButton " variant="outline-success">Search</Button>
           </Form>
+          <button className="btn btn-primary ml-auto" onClick={toggleModal}>Login</button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    <Modal isOpen={isModalOpen} toggle={toggleModal}>
+<ModalHeader isOpen={isModalOpen} toggle={toggleModal}>Login</ModalHeader>
+<ModalBody>
+<Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={toggleModal} >
+        Submit
+      </Button>
+    </Form>
+
+</ModalBody>
+    </Modal>
     
+    </>
    
    
    
@@ -53,7 +90,8 @@ import './header.css';
             
 
         );
-    }
- }export default Header;
+    
+ }
+ export default Header;
 
 
